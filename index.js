ea61471dcfc537ff71946a9711d51ece6b7ebb92
Jason Loomis <jason@jrloomis.com>
2019-11-01 11:28:35 -0400

    fixed spelling error, post -> port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
+console.log(process.env.MSG);
 
 const server = require("./server");
 const port = process.env.PORT;
 
-server.listen(port, () => console.log(`--> server running on port ${post}`));
+server.listen(port, () => console.log(`--> server running on port ${port}`));
